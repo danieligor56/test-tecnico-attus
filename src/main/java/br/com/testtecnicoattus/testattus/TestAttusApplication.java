2370cfc2b3f0652582035e5aba2b547658250ad2
My PC home <danieligorxd@gmail.com>
2024-04-22 23:53:03 -0300

    Subindo arquivos do projeto

---
+++ src/main/java/br/com/testtecnicoattus/testattus/TestAttusApplication.java
@@ -0,0 +1,13 @@
+package br.com.testtecnicoattus.testattus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAttusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAttusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/testtecnicoattus/testattus/TestAttusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.testtecnicoattus.testattus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAttusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
