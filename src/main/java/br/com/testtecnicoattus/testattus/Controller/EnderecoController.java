1189b00c9571fe51f0a080a67125e43600367478
My PC home <danieligorxd@gmail.com>
2024-04-29 01:04:45 -0300

    Documentação dos Endereços ajustada

--- src/main/java/br/com/testtecnicoattus/testattus/Controller/EnderecoController.java
+++ src/main/java/br/com/testtecnicoattus/testattus/Controller/EnderecoController.java
@@ -16,6 +16,7 @@ import br.com.testtecnicoattus.testattus.EnderecoServices.AdicionaNovoEndereco;
 import br.com.testtecnicoattus.testattus.EnderecoServices.AlteraEndereco;
 import br.com.testtecnicoattus.testattus.EnderecoServices.ListaEnderecos;
 import br.com.testtecnicoattus.testattus.Entity.Endereco;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 
 @Controller
@@ -33,11 +34,18 @@ public class EnderecoController {
 	@Autowired
 	ListaEnderecos listaEnderecos;
 	
+	@Operation(summary = "Adiciona novo endereço", description = "Adiciona um novo endereço a lista de endereços existentes")
 	@PostMapping("/adicionaEndereco")	
 	public ResponseEntity<Endereco> adicionaEndereco(@RequestBody @Valid List<EnderecoDTO> dto){
 		novoEndereco.adicionaNovoEndereco(dto);		
 		return ResponseEntity.ok().build();
 	}
+	@Operation(summary = "Altera endereço", description = "Recebe como parametro o Id referente ao endereço que deseja alterar"
+			+ "e um DTO da classe contendo as alterações. "
+			+ "Também pode ser utilizado para setar o tipo de endereços,sendo: "+"\n"
+			+"0 = PRINCIPAL,"+"\n"
+			+"1 = SECUNDARIO,"+"\n"
+			+"2 = TRABALHO.")
 	
 	@PutMapping("/alteraEndereco")	
 	public ResponseEntity<Endereco> alteraEndereco(@RequestParam List<Integer> idEndereco, @RequestBody List<EnderecoDTO> dto){
@@ -45,15 +53,20 @@ public class EnderecoController {
 		return ResponseEntity.ok().build();
 	}
 	
+	@Operation(summary = "Lista endereços", description = "Lista todos os endereços")
 	@GetMapping("/listaEnderecos")
 	public ResponseEntity<List<Endereco>> listaEnderecos(){
 		return ResponseEntity.ok().body(listaEnderecos.listaEnderecos());
 	}
+	@Operation(summary = "Lista endereços por pessoa", description = "Recebe como parametro um id referente a "
+			+ "uma pessoa e devolve todos os endereços pertencentes a ela")
 	
 	@GetMapping("/ListaPorPessoa")
 	public ResponseEntity<List<Endereco>> listaEnderecoPorPessoa(@RequestParam Integer id){
 		return ResponseEntity.ok().body(listaEnderecos.listaEnderecoPorPessoa(id));
 	}
+	@Operation(summary = "Encontrar um endereço por ID", description = "Recebe um ID do tipo Integer e devolve o endereço referente.")
+	
 	@GetMapping("/encontrarPorId")
 	public ResponseEntity<Endereco> encontraPorId(@RequestParam Integer id){
 		return ResponseEntity.ok().body(listaEnderecos.encontraPorId(id));
