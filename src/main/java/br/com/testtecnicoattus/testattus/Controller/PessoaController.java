d794755feebbb86ac5e040942249ca25884826d3
My PC home <danieligorxd@gmail.com>
2024-04-29 01:12:17 -0300

    Alteração de documentção referente a Pessoa ajustada

--- src/main/java/br/com/testtecnicoattus/testattus/Controller/PessoaController.java
+++ src/main/java/br/com/testtecnicoattus/testattus/Controller/PessoaController.java
@@ -12,11 +12,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import br.com.testtecnicoattus.testattus.Dto.PessoaDTO;
 import br.com.testtecnicoattus.testattus.Entity.Pessoa;
 import br.com.testtecnicoattus.testattus.PessoaServices.AdicionaNovaPessoa;
 import br.com.testtecnicoattus.testattus.PessoaServices.AlteraPessoaExistente;
 import br.com.testtecnicoattus.testattus.PessoaServices.EncontraPessoa;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 
 @Controller
@@ -33,29 +35,31 @@ public class PessoaController {
 	
 	@Autowired
 	EncontraPessoa encontraPessoa;
-	
+
+
+	@Operation(summary = "Adiciona uma ou mais pessoas.", description = "Adiciona novas pessoas a lista de pessoas existentes")
 	@PostMapping("/adicionaPessoa")	
 	public ResponseEntity<Pessoa> adicionarPessoa(@RequestBody @Valid List<PessoaDTO> pessoaDTO) throws Exception{
 		adicionaNovaPessoa.adicionaNovaPessoa(pessoaDTO);		
 		return ResponseEntity.ok().build();
 	}
-	
+	@Operation(summary = "Altera o registro de uma pessoa ou mais ", description = "Recebe como parametro um ID e um dto contendo as alterações")
 	@PutMapping("/alteraPessoa")
 	public ResponseEntity<Pessoa> alteraPessoa(@RequestParam List<Integer>id, @RequestBody List<PessoaDTO> pessoaDto) throws Exception {
 		alteraPessoaExistente.alteraPessoa(id, pessoaDto);
 		return ResponseEntity.ok().build();
 	}
-	
+	@Operation(summary = "Encontra um registro de Pessoa por ID ", description = "Encontra um registro de pessoa utilizando o ID como parametro")
 	@GetMapping("/encontraPessoa/{id}")
-	public ResponseEntity<Pessoa> encontrPesso(@RequestParam Integer id) {
+	public ResponseEntity<Pessoa> encontraPessoa(@RequestParam Integer id) {
 		return ResponseEntity.ok().body(encontraPessoa.encontrarPessoa(id));
 	}
-	
+	@Operation(summary = "Encontra registros por nome.", description = "Encontra registros utilizando o nome ou o fragmentos de um nome")
 	@GetMapping("/encontraPessoaNome/")
 	public ResponseEntity<List<Pessoa>> encontrPessoPorNome(@RequestParam String nome)  {
 		return ResponseEntity.ok().body(encontraPessoa.encontrarPorNome(nome));
 	}
-	
+	@Operation(summary = "Lista os registros existentes", description = "Lista todos os registros de Pessoa existentes")
 	@GetMapping("/listarPessoa/")
 	public ResponseEntity<List<Pessoa>> listarPessoas() {
 		return ResponseEntity.ok().body(encontraPessoa.listarPessoas());
